7de96db66eb96f09f55a704011fe731e0e696cd6
mhaxanali <mhasanali2010@gmail.com>
2026-01-19 13:08:04 +0500

    fix typos

--- zzmath/evaluate.cpp
+++ zzmath/evaluate.cpp
@@ -9,7 +9,7 @@ void handleUnaryMinus(strVec_t& tokens);
 strVec_t tokenize(std::string expr);
 bool contains(const strVec_t& compSlice, const std::string& comp_str);
 void solveTokens(strVec_t& tokens);
-double toDouble(const str::string& str);
+double toDouble(const std::string& str);
 void solve(strVec_t& tokens, int i);
 void solveInnerMostBrackets(strVec_t& tokens);
 
