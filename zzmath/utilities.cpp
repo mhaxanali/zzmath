b34101a3fbc762ac271a0bbf69d9f565f75ebaa2
mhaxanali <mhasanali2010@gmail.com>
2026-05-31 20:44:56 +0500

    Add function definition for count and change function declaration for contains util funcs

--- zzmath/utilities.cpp
+++ zzmath/utilities.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-bool contains(std::vector<std::string> vector, std::string string) {
+bool contains(const std::vector<std::string>& vector, const std::string& string) {
 
     for (const std::string &item : vector) {
         if (item == string) {
@@ -9,4 +9,14 @@ bool contains(std::vector<std::string> vector, std::string string) {
         }
     }
     return false;
+}
+
+int count (const std::vector<std::string>& col, const std::string& com) {
+    int count = 0;
+    for (const std::string &v : col) {
+        if (v == com) {
+            count++;
+        }
+    }
+    return count;
 }
